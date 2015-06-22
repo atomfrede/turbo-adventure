d4d546aa7c73137f0e8447758550dc57cb8fc9ff
Frederik Hahne <atomfrede@gmail.com>
2015-06-22 23:01:08 +0200

    Add triple

---
+++ src/main/java/de/atomfrede/github/karaoke/server/entity/Triple.java
@@ -0,0 +1,7 @@
+package de.atomfrede.github.karaoke.server.entity;
+
+public class Triple {
+
+    private Singer left, right;
+    private Song songToSing;
+}
---
+++ src/main/java/de/atomfrede/github/karaoke/server/service/PairingService.java
@@ -0,0 +1,11 @@
+package de.atomfrede.github.karaoke.server.service;
+
+import de.atomfrede.github.karaoke.server.entity.Triple;
+
+public class PairingService {
+
+    public Triple getNextRandomPair() {
+
+        return null;
+    }
+}
