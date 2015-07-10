541b362141b987c6ed216bd93d74828622610f35
Frederik Hahne <atomfrede@users.noreply.github.com>
2015-07-10 15:25:47 +0200

    Update PingResource.java

--- src/main/java/de/atomfrede/github/karaoke/server/resource/PingResource.java
+++ src/main/java/de/atomfrede/github/karaoke/server/resource/PingResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Path("/ping")
 @Produces(MediaType.APPLICATION_JSON)
