c8be141e91e0a8c372826aff936bda5e23c65fee
Frederik Hahne <atomfrede@users.noreply.github.com>
2015-07-10 15:15:32 +0200

    Update Pong.java

--- src/main/java/de/atomfrede/github/karaoke/server/entity/Pong.java
+++ src/main/java/de/atomfrede/github/karaoke/server/entity/Pong.java
@@ -3,4 +3,9 @@ package de.atomfrede.github.karaoke.server.entity;
 public class Pong {
 
     public String msg = "Pong";
+    
+    public Pong(String msg) {
+        
+        this.msg = msg;
+    }
 }
