c13b3e8050ee8a10951560408da366d0367ed846
Frederik Hahne <atomfrede@users.noreply.github.com>
2015-07-10 15:24:01 +0200

    Update PingResource.java

--- src/main/java/de/atomfrede/github/karaoke/server/resource/PingResource.java
+++ src/main/java/de/atomfrede/github/karaoke/server/resource/PingResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.InetAddress;
 
 @Path("/ping")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,7 +18,7 @@ public class PingResource {
     @GET
     @Timed
     @Path("pong")
-    public Pong pong() {
+    public Pong pong() throws UnknownHostException {
         return new Pong(InetAddress.getLocalHost().getHostName() + "");
     }
 }
