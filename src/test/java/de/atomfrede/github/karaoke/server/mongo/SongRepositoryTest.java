4ab011f3d4e6b4b6a33033fd4fa23a19ce74ca22
Sascha Rinne <sascha.rinne@wallmedien.de>
2015-11-27 12:53:33 +0100

    Fix SongRepositoryTest.

--- src/test/java/de/atomfrede/github/karaoke/server/mongo/SongRepositoryTest.java
+++ src/test/java/de/atomfrede/github/karaoke/server/mongo/SongRepositoryTest.java
@@ -4,18 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import java.util.Arrays;
-import de.atomfrede.github.karaoke.server.entity.Song;
-import org.jongo.Jongo;
-import org.jongo.MongoCollection;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import com.mongodb.DB;
+import de.atomfrede.github.karaoke.server.entity.Song;
 import mockit.Mocked;
 import mockit.NonStrictExpectations;
 import mockit.integration.junit4.JMockit;
+import org.bson.types.ObjectId;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(JMockit.class)
 public class SongRepositoryTest {
@@ -31,23 +31,20 @@ public class SongRepositoryTest {
 
 	@Before
 	public void setUp() {
-		songRepository = new SongRepository(database);
+
 		//Initialisierung der Instanzvariablen
+		songRepository = new SongRepository(database);
 	}
 
 	@Test
 	public void assertThatCountIsReturned() {
 
-		new NonStrictExpectations() {
-			{//einen bestimmten Wert zurückgeben
-				
-				mongoCollection.count();
-				// Methode count der Klasse mongoCollection wird initialisiert
-				times = 1;
-				// wird genau einmal ausgeführt
-				result = 24;
-			} 
-		};
+		new NonStrictExpectations() {{//einen bestimmten Wert zurückgeben
+
+			// Methode count der Klasse mongoCollection wird initialisiert und genau einmal ausgeführt
+			mongoCollection.count();
+			times = 1; result = 24;
+		}};
 
 		assertThat(songRepository.count(), is(24L));
 		// assertThat (x, is(24)) 
@@ -56,34 +53,30 @@ public class SongRepositoryTest {
 	@Test
 	public void assertThatEntityIsRemovedById() {
 
-		new NonStrictExpectations() {
-			{
-				mongoCollection.remove("{_id:#}", "24");
-				times = 1;
-			}
-		};
+		new NonStrictExpectations() {{
 
-		songRepository.delete("24");
+			mongoCollection.remove("{_id:#}", new ObjectId("507f191e810c19729de860ea"));
+			times = 1;
+		}};
+
+		songRepository.delete("507f191e810c19729de860ea");
 		// deleteMethode aufrufen, id 24 löschen
 	}
 
 	@Test
 	public void assertThatEntitiesAreRemovedById() {
 
-		new NonStrictExpectations() {
-			{
+		new NonStrictExpectations() {{
 
-				mongoCollection.remove("{_id:#}", "24");
-				times = 1;
+			mongoCollection.remove("{_id:#}", "507f191e810c19729de860ea");
+			times = 1;
 
-				mongoCollection.remove("{_id:#}", "71");
-				times = 1;
-			}
-		};
+			mongoCollection.remove("{_id:#}", "507f191e810c19729de860eb");
+			times = 1;
+		}};
 
-		Song SongA = new Song("24").setTitle("SongA").setInterpreter("InterpreterA");
-
-		Song SongB = new Song("71").setTitle("SongA").setInterpreter("InterpreterB");
+		Song SongA = new Song("507f191e810c19729de860ea").setTitle("SongA").setInterpreter("InterpreterA");
+		Song SongB = new Song("507f191e810c19729de860eb").setTitle("SongA").setInterpreter("InterpreterB");
 
 		songRepository.delete(Arrays.asList(SongA, SongB));
 	}
@@ -91,15 +84,12 @@ public class SongRepositoryTest {
 	@Test
 	public void assertThatAllEntitiesAreRemoved() {
 
-		new NonStrictExpectations() {
-			{
+		new NonStrictExpectations() {{
 
-				mongoCollection.drop();
-				// Drops this collection from the Database - verwirft die
-				// gesamte Sammlung
-				times = 1;
-			}
-		};
+			// Drops this collection from the Database - verwirft die gesamte Sammlung
+			mongoCollection.drop();
+			times = 1;
+		}};
 
 		songRepository.deleteAll();
 	}
@@ -107,23 +97,18 @@ public class SongRepositoryTest {
 	@Test
 	public void assertThatOneCanBeFound() {
 
-		new NonStrictExpectations() {
-			{
+		new NonStrictExpectations() {{
 
-				mongoCollection.findOne("{_id:#}", "24").as(Song.class);
-				// Returns one document that satisfies the specified query
-				// criteria
-				times = 1;
-				result = new Song("24");
+			// Returns one document that satisfies the specified query criteria
+			mongoCollection.findOne("{_id:#}", new ObjectId("507f191e810c19729de860ea")).as(Song.class);
+			times = 1; result = new Song("24");
 
-				mongoCollection.findOne("{_id:#}", "71").as(Song.class);
-				times = 1;
-				result = null;
-			}
-		};
+			mongoCollection.findOne("{_id:#}", new ObjectId("507f191e810c19729de860eb")).as(Song.class);
+			times = 1; result = null;
+		}};
 
-		assertThat(songRepository.exists("24"), is(true));
-		assertThat(songRepository.exists("71"), is(false));
+		assertThat(songRepository.exists("507f191e810c19729de860ea"), is(true));
+		assertThat(songRepository.exists("507f191e810c19729de860eb"), is(false));
 	}
 	
 }
